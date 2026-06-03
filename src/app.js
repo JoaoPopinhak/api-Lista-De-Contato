3738aa4df97697834af271271718162c755fcd1e
JoaoPopinhak <joaomrpopinhak@hotmail.com>
2026-06-03 17:52:32 -0300

    Criado rota para salvar novo contato

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import helmet from 'helmet';
 import routers from './routes/index.js';
 
+//Criando o servidor
 const app = express();
+//Configurações
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+
+//Rotas
 app.use('/', routers);
 
 app.listen(3000, () => {
-
     console.log('Servidor rodando...');
-
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import { readFile, writeFile } from 'fs/promises';
+
 
 const routers = express.Router();
 
-routers.get('/', (req, res) => {
-    res.json({description: 'API de Lista de Contatos'});
+
+const dataPath = './data/list.txt'
+routers.post('/contato', async (req, res) => {
+    const {name} = req.body || {};
+
+    if(!name){
+        return res.json({error: 'Nome não preenchido'});
+    }
+    //Processamento dos dados
+    let listDados = [];
+    try{
+        const dados = await readFile(dataPath, 'utf-8');
+        listDados = dados.split('\n');
+        
+    }catch(err){}
+    listDados.push(name); 
+    
+    await writeFile(dataPath, listDados.join('\n'));;
+    
+    res.status(201).json({contato: name});
 });
 
 export default routers;
