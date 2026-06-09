c16606d89ccd2c8656ea74ecfdbe5162ab5757ae
JoaoPopinhak <joaomrpopinhak@hotmail.com>
2026-06-09 14:45:02 -0300

    Atualização

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,13 +17,42 @@ routers.post('/contato', async (req, res) => {
     try{
         const dados = await readFile(dataPath, 'utf-8');
         listDados = dados.split('\n');
-        
     }catch(err){}
     listDados.push(name); 
     
     await writeFile(dataPath, listDados.join('\n'));;
     
     res.status(201).json({contato: name});
+});
+
+routers.get('/contatos', async (req,res) => {
+    let listDados = [];
+    try{
+        const dados = await readFile(dataPath, 'utf-8');
+        listDados = dados.split('\n')
+    }catch(err){}
+
+    res.json({Contatos: listDados});
+});
+
+routers.delete('/contato', async (req,res) => {
+    const { name } = req.query;
+
+    if(!name){
+        return res.json({error: 'Precisa informar o nome que deseja deletar'});
+    }
+
+    let listDados = [];
+    try{
+        const dados = await readFile(dataPath, 'utf-8');
+        listDados = dados.split('\n');
+    }catch(err){}
+
+    listDados = listDados.filter(item => (item.toLowercase() !== name.toLowerCase()));
+
+    await writeFile(dataPath, listDados.join('\n'));
+
+    res.json({message: `Contato ${name} deletado com sucesso`});
 });
 
 export default routers;
